3bb2800659c1a6a92c86292f9a2962fcf6679438
Lech <88630083+Artemka374@users.noreply.github.com>
2026-01-07 17:15:14 +0100

    Revert "Merge branch 'sb-tmp-hacky-v29-interop' into afo/interop-dev"
    This reverts commit a60b6ab6fac2ef73c9b7417bf4150fb4e0bfd2d3, reversing
    changes made to 682fa3fcd7e6f4dc1a20c6f33fa27facdd188bf5.

--- node/bin/src/config_constants.rs
+++ node/bin/src/config_constants.rs
@@ -8,11 +8,11 @@ pub const DEFAULT_ROCKS_DB_PATH: &str = "./db/node1";
 
 /// L1 address of `Bridgehub` contract. This address and chain ID is an entrypoint into L1 discoverability so most
 /// other contracts should be discoverable through it.
-pub const BRIDGEHUB_ADDRESS: &str = "0x6300a5e20ab03c24975d07970db6f9b6184f8a08";
+pub const BRIDGEHUB_ADDRESS: &str = "0x8aeec25f84d73c079b318881e445404a3b0cd2d2";
 
 /// L1 address of the `BytecodeSupplier` contract. This address right now cannot be discovered through `Bridgehub`,
 /// so it has to be provided explicitly.
-pub const BYTECODE_SUPPLIER_ADDRESS: &str = "0x48f8682816ba6c6b7e366be2d206ed76c8389d43";
+pub const BYTECODE_SUPPLIER_ADDRESS: &str = "0xabf0447fd9281124968e259e9b9062041e0c98fc";
 
 /// Chain ID of the chain node operates on.
 pub const CHAIN_ID: u64 = 6565;
@@ -20,14 +20,14 @@ pub const CHAIN_ID: u64 = 6565;
 /// Private key to commit batches to L1
 /// Must be consistent with the operator key set on the contract (permissioned!)
 #[rustfmt::skip]
-pub const OPERATOR_COMMIT_PK: &str = "0x9636724f377116fabebc1b34300db0caa7e4384ab8da78473952ba2898b1e665";
+pub const OPERATOR_COMMIT_PK: &str = "0x2c096b29fdfa333428eca566906cc8c89ecf715f99d15fd94967509a240c4de2";
 
 /// Private key to use to submit proofs to L1
 /// Can be arbitrary funded address - proof submission is permissionless.
 #[rustfmt::skip]
-pub const OPERATOR_PROVE_PK: &str = "0x3c6003f8773737763f1f9456479dfc45f7dca89b5ff536397faf545363490c31";
+pub const OPERATOR_PROVE_PK: &str = "0xb02977208bae4f76545b488dc1964ba3afa014b8bb14963a1e20dc1ebde07579";
 
 /// Private key to use to execute batches on L1
 /// Can be arbitrary funded address - execute submission is permissionless.
 #[rustfmt::skip]
-pub const OPERATOR_EXECUTE_PK: &str = "0x0adbd235f0a73d266b3bc1bf9190f3ff71da43bc03fbdfb0529069718b7e0629";
+pub const OPERATOR_EXECUTE_PK: &str = "0xf2658e1bd579b90a9ffe659ddcc76b6a178db1692aae304bb2ea01c3a1fa651a";
